d711eca7bad98e9c477e7a3a3d9b964641384463
SlaySlash <piorunjakubwot@gmail.com>
2026-09-08 19:22:39 +0200

    added fetch, simple style,header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import React from "react"
+import Header from "./components/header";
 
 export default function App() {
-    
+    const [question, setQuestion] = React.useState([])
+    React.useEffect(() =>{
+        const url = import.meta.env.VITE_QUIZ_URL;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const mapQuestion = data.results.map((q, index) => 
+                ({question: q.question,
+                  id: index,
+                  category: q.category,
+                  answers: [...q.incorrect_answers, q.correct_answer],
+                  selectedAnswer: null, 
+                  correctAnswer: q.correct_answer
+                }))
+            setQuestion(mapQuestion)
+        })
+    }
+    , [])
 
+    
     return (
         <main>
-            
+            <Header />
+            <section className="question-container">
+                <h2></h2>
+                <p></p>
+            </section>
         </main>
     )
 }
