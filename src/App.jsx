898aba74f741a10bcb49a77410804e0269ac0489
SlaySlash <piorunjakubwot@gmail.com>
2026-09-08 20:06:17 +0200

    added sort and html decoder

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import Header from "./components/header";
 
 export default function App() {
     const [question, setQuestion] = React.useState([])
+
+
+    function decode(text){
+        const el = document.createElement("textarea")
+        el.innerHTML = text
+        return el.value
+    }
     React.useEffect(() =>{
         const url = import.meta.env.VITE_QUIZ_URL;
         fetch(url)
         .then(res => res.json())
         .then(data => {
             const mapQuestion = data.results.map((q, index) => 
-                ({question: q.question,
+                ({question: decode(q.question),
                   id: index,
                   category: q.category,
-                  answers: [...q.incorrect_answers, q.correct_answer],
+                  answers: [...q.incorrect_answers, q.correct_answer].map(decode).sort(() => Math.random() - 0.5),
                   selectedAnswer: null, 
-                  correctAnswer: q.correct_answer
+                  correctAnswer: decode(q.correct_answer)
                 }))
             setQuestion(mapQuestion)
         })
@@ -22,13 +29,20 @@ export default function App() {
     , [])
     const readyQuestion = question.map(q =>{
         return(
-            <>
-                <h2 key={q.id}>{q.question}</h2>
-                <p key={q.id}>{q.answers}</p>
-            </>
+            <div key={q.id} className="question">
+                <h2>{q.question}</h2>
+                {q.answers.map(a =>{
+                    return (
+                        <button key={a}>
+                            {a}
+                        </button>
+                    )
+                }
+                )}
+            </div>
         )
     })
-    
+
     return (
         <main>
             <Header />
